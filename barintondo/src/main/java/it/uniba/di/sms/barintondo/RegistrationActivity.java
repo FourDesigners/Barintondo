dbe9203d75715ea6fc62f842a6cfc8f9b9e9a5c0
ddipierro <ddipierro745@gmail.com>
2018-12-31 12:08:29 +0100

    Updated registration

--- barintondo/src/main/java/it/uniba/di/sms/barintondo/RegistrationActivity.java
+++ barintondo/src/main/java/it/uniba/di/sms/barintondo/RegistrationActivity.java
@@ -1,19 +1,27 @@
 package it.uniba.di.sms.barintondo;
 
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
+import it.uniba.di.sms.barintondo.utils.Constants;
+import it.uniba.di.sms.barintondo.utils.ProfileOpenHelper;
+import it.uniba.di.sms.barintondo.utils.VerifyString;
+
+import static java.security.AccessController.getContext;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    TextView textViewNicknameError, textViewEmailError, textViewPasswordError, textViewRepeatPasswordError;
     EditText editTextNickname, editTextEmail, editTextPassword, editTextRepeatPassword;
     ImageView imageView, imageView2;
     Button reset, register;
@@ -23,12 +31,30 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        android.support.v7.widget.Toolbar myToolbar = findViewById(R.id.toolbar);
+        textViewNicknameError = findViewById(R.id.textViewNicknameError);
+        textViewEmailError = findViewById(R.id.textViewEmailError);
+        textViewPasswordError = findViewById(R.id.textViewPasswordError);
+        textViewRepeatPasswordError = findViewById(R.id.textViewRepeatPasswordtError);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
         myToolbar.setTitle(R.string.str_register);
         setSupportActionBar(myToolbar);
         ActionBar actionbar = getSupportActionBar();
         assert actionbar != null; //serve per non far apparire il warning che dice che actionbar potrebbe essere null
         actionbar.setDisplayHomeAsUpEnabled(true);
+
+        /////
+
+        ProfileOpenHelper dbHelper = new ProfileOpenHelper(this, Constants.DB_NAME, null, 1);
+        SQLiteDatabase myDB = dbHelper.getWritableDatabase();
+        ContentValues user = new ContentValues();
+        user.put(Constants.COLUMN_USERNAME, "prova2");
+        user.put(Constants.COLUMN_EMAIL, "pinco@prova.it");
+        user.put(Constants.COLUMN_PASSWORD, "prova1");
+        long newID = myDB.insert(Constants.TABLE_UTENTE, null, user);
+
+        /////
+
 
         editTextNickname = findViewById(R.id.editTextNickname);
 
@@ -109,9 +135,72 @@ public class RegistrationActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String nickname = editTextNickname.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String repeatPassword = editTextRepeatPassword.getText().toString();
+                boolean correct = isCorrect(nickname, email, password, repeatPassword);
+                if(!isPresent(email)) {
+                    insertInto(nickname, email, password);
+                }
             }
         });
 
+    }
+
+    private boolean isPresent(String email) {
+
+        return false;
+    }
+
+    private void insertInto(String nickname, String email, String password) {
+        /*
+        ProfileOpenHelper openHelper = new ProfileOpenHelper(getApplicationContext(), Constants.DB_NAME, null, 1);
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Constants.COLUMN_NICKNAME, nickname);
+        values.put(Constants.COLUMN_EMAIL, email);
+        values.put(Constants.COLUMN_PASSWORD, password);
+        db.insert(Constants.TABLE_UTENTE, null, values);
+        db.close();
+        */
+        ProfileOpenHelper dbHelper = new ProfileOpenHelper(this, Constants.DB_NAME, null, 1);
+        SQLiteDatabase myDB = dbHelper.getWritableDatabase();
+        ContentValues user = new ContentValues();
+        user.put(Constants.COLUMN_USERNAME, "prova2");
+        user.put(Constants.COLUMN_EMAIL, "pinco@prova.it");
+        user.put(Constants.COLUMN_PASSWORD, "prova1");
+        long newID = myDB.insert(Constants.TABLE_UTENTE, null, user);
+        Toast.makeText(getApplicationContext(), "Account creato", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isCorrect(String nickname, String email, String password, String repeatPassword) {
+        boolean correct = true;
+
+        if(VerifyString.isNicknameNotValid(nickname)) {
+            textViewNicknameError.setText(R.string.str_nickname_is_empty);
+            correct = false;
+        }else {
+            textViewNicknameError.setText("");
+        }
+        if(VerifyString.isEmailNotValid(email)) {
+            textViewEmailError.setText(R.string.str_email_not_valid);
+            correct = false;
+        }else {
+            textViewEmailError.setText("");
+        }
+        if(VerifyString.isPasswordNotValid(password)) {
+            textViewPasswordError.setText(R.string.str_weak_password_min_8_characters_required);
+            correct = false;
+        }else {
+            textViewPasswordError.setText("");
+        }
+        if(VerifyString.notMatchingPasswords(password, repeatPassword)) {
+            textViewRepeatPasswordError.setText(R.string.str_passwords_don_t_match);
+            correct = false;
+        }else {
+            textViewRepeatPasswordError.setText("");
+        }
+        return correct;
     }
 }
---
+++ barintondo/src/main/java/it/uniba/di/sms/barintondo/utils/VerifyString.java
@@ -0,0 +1,31 @@
+package it.uniba.di.sms.barintondo.utils;
+
+import java.util.regex.Pattern;
+
+public class VerifyString {
+    private static final int MIN_LENGTH = 8;
+
+    public static boolean isNicknameNotValid(String nickname) {
+        return nickname.equals("");
+    }
+
+    public static boolean isEmailNotValid(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return true;
+        return !pat.matcher(email).matches();
+    }
+
+    public static boolean isPasswordNotValid(String password) {
+        return password.length() < MIN_LENGTH;
+    }
+
+    public static boolean notMatchingPasswords(String password1, String password2) {
+        return !password1.equals(password2);
+    }
+}
