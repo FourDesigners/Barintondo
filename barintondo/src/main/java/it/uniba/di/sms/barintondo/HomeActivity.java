04f216e16c74a44051f78c9d3326f8bd55bcc695
Dom976 <dom.gigante0@gmail.com>
2019-01-03 17:04:35 +0100

    Weather widget onClick and network check added

--- barintondo/src/main/java/it/uniba/di/sms/barintondo/HomeActivity.java
+++ barintondo/src/main/java/it/uniba/di/sms/barintondo/HomeActivity.java
@@ -1,6 +1,8 @@
 package it.uniba.di.sms.barintondo;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -36,25 +38,14 @@ public class HomeActivity extends AppCompatActivity implements Constants {
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, getClass().getSimpleName() + ":entered onCreate()");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home );
+        setContentView(R.layout.activity_home);
 
 
-        fillChips();            // Inserimento filtri
-
-        /*
-        //popolamento db
-        ProfileOpenHelper dbHelper = new ProfileOpenHelper(this, Constants.DB_NAME, null, 1);
-        SQLiteDatabase myDB = dbHelper.getWritableDatabase();
-        ContentValues user = new ContentValues();
-        user.put(Constants.COLUMN_NICKNAME, "prova1");
-        user.put(Constants.COLUMN_EMAIL, "pinco@prova.it");
-        user.put(Constants.COLUMN_PASSWORD, "prova1");
-        long newID = myDB.insert(Constants.TABLE_UTENTE, null, user);
-        */
+        fillChips(); // Inserimento filtri
 
         myNavigationDrawer = new MyNavigationDrawer(this,
                 (NavigationView) findViewById(R.id.nav_view),
-                (DrawerLayout )findViewById(R.id.drawer_layout));
+                (DrawerLayout) findViewById(R.id.drawer_layout));
         myNavigationDrawer.build();
 
         myToolbar = findViewById(R.id.main_activity_toolbar);
@@ -72,7 +63,7 @@ public class HomeActivity extends AppCompatActivity implements Constants {
 
     private void fillChips() {
         SharedPreferences categories = getSharedPreferences(PREFS_NAME, 0);
-        if(!categories.contains(INSERTED)) {
+        if (!categories.contains(INSERTED)) {
             SharedPreferences.Editor edit = categories.edit();
             edit.putBoolean(INSERTED, true);
             edit.putString(INTENT_ATTRACTIONS, CHIPS_ATTRACTIONS);
@@ -87,8 +78,8 @@ public class HomeActivity extends AppCompatActivity implements Constants {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Log.i(TAG, getClass().getSimpleName() + ":entered onOptionsItemSelected()");
-        Boolean open = myNavigationDrawer.openMenu( item );
-        if(open) return open;
+        Boolean open = myNavigationDrawer.openMenu(item);
+        if (open) return open;
         else return super.onOptionsItemSelected(item);
     }
 
@@ -97,46 +88,55 @@ public class HomeActivity extends AppCompatActivity implements Constants {
         super.onResume();
 
         //METEO
-        helper = new OpenWeatherMapHelper(); //creo oggetto helper
-        helper.setApiKey(getString(R.string.OPEN_WEATHER_MAP_API_KEY)); //imposto l'API KEY
-        helper.setUnits(Units.METRIC); //imposto l'unità in modo che mostri i gradi in CELSIUS
-        Log.i(TAG, "lang= " + Locale.getDefault().getLanguage());
-        helper.setLang(Locale.getDefault().getLanguage()); //imposto lingua delle info in base alla lingua del s.o.
-        //ottengo info
-        helper.getCurrentWeatherByGeoCoordinates(Double.parseDouble(getResources().getString(R.string.bari_latitude)), Double.parseDouble(getResources().getString(R.string.bari_longitude)), new OpenWeatherMapHelper.CurrentWeatherCallback() {
-            @Override
-            public void onSuccess(CurrentWeather currentWeather) {
-                Log.i(TAG,
-                        "Weather Description: " + currentWeather.getWeatherArray().get(0).getDescription() + "\n"
-                                +"Temp: "+currentWeather.getMain().getTemp() +"\n"
-                                +"Wind Speed: " + currentWeather.getWind().getSpeed() + "\n"
-                );
+        boolean connected = InternetConnection.isNetworkAvailable(this);
+        if (connected) { //controllo che sia disponibile la connessione internet
+            helper = new OpenWeatherMapHelper(); //creo oggetto helper
+            helper.setApiKey(getString(R.string.OPEN_WEATHER_MAP_API_KEY)); //imposto l'API KEY
+            helper.setUnits(Units.METRIC); //imposto l'unità in modo che mostri i gradi in CELSIUS
+            Log.i(TAG, "lang= " + Locale.getDefault().getLanguage());
+            helper.setLang(Locale.getDefault().getLanguage()); //imposto lingua delle info in base alla lingua del s.o.
+            //ottengo info tramite API
+            helper.getCurrentWeatherByGeoCoordinates(Double.parseDouble(getResources().getString(R.string.bari_latitude)), Double.parseDouble(getResources().getString(R.string.bari_longitude)), new OpenWeatherMapHelper.CurrentWeatherCallback() {
+                @Override
+                public void onSuccess(CurrentWeather currentWeather) {
+                    Log.i(TAG,
+                            "Weather Description: " + currentWeather.getWeatherArray().get(0).getDescription() + "\n"
+                                    + "Temp: " + currentWeather.getMain().getTemp() + "\n"
+                                    + "Wind Speed: " + currentWeather.getWind().getSpeed() + "\n"
+                    );
 
-                //imposto temperatura
-                TextView temp = findViewById(R.id.temp);
-                temp.setText(String.valueOf(Math.round(currentWeather.getMain().getTemp())).concat(getResources().getString(R.string.tempUnit)));
-                //ottengo url icona
-                String iconurl = "http://openweathermap.org/img/w/" + currentWeather.getWeatherArray().get(0).getIcon() + ".png";
-                //imposto icona
-                Glide.with(HomeActivity.this)
-                        .load(iconurl)
-                        .apply(new RequestOptions().override(144, 144)) //resize immagine altrimenti troppo piccola
-                        .into( (ImageView) findViewById(R.id.weatherIcon));
-                //imposto descrizione
-                TextView desc = findViewById(R.id.weatherDesc);
-                String formattedString = capitalizeFirstLetter(String.valueOf(currentWeather.getWeatherArray().get(0).getDescription()));
-                desc.setText(formattedString);
-                //imposto velocità vento
-                /*TextView windSpeed = findViewById(R.id.windSpeed);
-                windSpeed.setText(String.valueOf(currentWeather.getWind().getSpeed()).concat(getResources().getString(R.string.windUnit)));
-                */
-            }
+                    //imposto temperatura
+                    TextView temp = findViewById(R.id.temp);
+                    temp.setText(String.valueOf(Math.round(currentWeather.getMain().getTemp())).concat(getResources().getString(R.string.tempUnit)));
+                    //ottengo url icona
+                    String iconurl = "http://openweathermap.org/img/w/" + currentWeather.getWeatherArray().get(0).getIcon() + ".png";
+                    //imposto icona
+                    Glide.with(HomeActivity.this)
+                            .load(iconurl)
+                            .apply(new RequestOptions().override(144, 144)) //resize immagine altrimenti troppo piccola
+                            .into((ImageView) findViewById(R.id.weatherIcon));
+                    //imposto descrizione
+                    TextView desc = findViewById(R.id.weatherDesc);
+                    String formattedString = capitalizeFirstLetter(String.valueOf(currentWeather.getWeatherArray().get(0).getDescription()));
+                    desc.setText(formattedString);
+                    //mostro btn
+                    Button moreInfoBtn = findViewById(R.id.moreBtn);
+                    moreInfoBtn.setVisibility(View.VISIBLE);
+                }
 
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.i(TAG, throwable.getMessage());
-            }
-        });
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    Log.i(TAG, throwable.getMessage());
+                }
+            });
+        }
+        else { //internet non disponibile
+            TextView desc = findViewById(R.id.weatherDesc);
+            desc.setText(getResources().getString(R.string.weatherNotAvailableMsg));
+            Button moreInfoBtn = findViewById(R.id.moreBtn);
+            moreInfoBtn.setVisibility(View.GONE);
+        }
     }
 
     //metodo necessario in quanto le API utilizzate restituiscono una string senza lettere maiuscole
@@ -150,7 +150,9 @@ public class HomeActivity extends AppCompatActivity implements Constants {
     private View.OnClickListener moreBtnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //TODO
+            Intent webWeatherIntent = new Intent(Intent.ACTION_VIEW);
+            webWeatherIntent.setData(Uri.parse(getResources().getString(R.string.webUrl)));
+            startActivity(webWeatherIntent);
         }
     };
 }
