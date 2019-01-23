d9b62ff79de153709cc3add0dfbb484d2e6bd076
Castellano <grazianocas90@gmail.com>
2019-01-23 12:34:47 +0100

    ItemDetailActivity aggiunto intent a googleMaps

--- barintondo/src/main/java/it/uniba/di/sms/barintondo/ItemDirectionsFragment.java
+++ barintondo/src/main/java/it/uniba/di/sms/barintondo/ItemDirectionsFragment.java
@@ -1,10 +1,13 @@
 package it.uniba.di.sms.barintondo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import it.uniba.di.sms.barintondo.utils.Constants;
@@ -13,6 +16,7 @@ public class ItemDirectionsFragment extends Fragment implements Constants {
 
     private String itemDirections;
     TextView textViewDirections;
+    ImageView iconGMaps;
 
     public ItemDirectionsFragment() {
         // Required empty public constructor
@@ -28,6 +32,7 @@ public class ItemDirectionsFragment extends Fragment implements Constants {
             // to load content from a content provider.
             itemDirections = getArguments().getString( ITEM_DIRECTIONS );
         }
+
     }
 
     @Override
@@ -37,12 +42,22 @@ public class ItemDirectionsFragment extends Fragment implements Constants {
         View rootView = inflater.inflate( R.layout.fragment_item_directions , container , false );
 
         // Show description as text in a TextView.
-        textViewDirections = rootView.findViewById(R.id.text_item_directions);
+        textViewDirections = rootView.findViewById( R.id.text_item_directions );
         textViewDirections.setText( itemDirections );
+
+        iconGMaps = rootView.findViewById( R.id.icon_gmaps );
+        iconGMaps.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse( "geo:0,0?q=" + itemDirections );
+                Intent mapIntent = new Intent( Intent.ACTION_VIEW , gmmIntentUri );
+                mapIntent.setPackage( "com.google.android.apps.maps" );
+                startActivity( mapIntent );
+            }
+        } );
 
         return rootView;
     }
-
 
 
     @Override
