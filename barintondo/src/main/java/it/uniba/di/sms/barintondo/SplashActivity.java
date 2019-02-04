bc99ba81db283625a37a7961e1f1a32fd3b9dcb2
ddipierro <ddipierro745@gmail.com>
2019-02-04 19:56:24 +0100

    Inserted SplashActivity

---
+++ barintondo/src/main/java/it/uniba/di/sms/barintondo/SplashActivity.java
@@ -0,0 +1,94 @@
+package it.uniba.di.sms.barintondo;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.lang.ref.WeakReference;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long INTERVAL = 2000L;
+    private static final int GO_AHEAD_WHAT = 1;
+
+    private long mStartTime = -1L;
+    private boolean mIsDone;
+    private UIHandler mHandler;
+
+    private static class UIHandler extends Handler {
+        private WeakReference<SplashActivity> mActivityRef;
+
+        public UIHandler(final SplashActivity srcActivity) {
+            this.mActivityRef = new WeakReference<>(srcActivity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            final SplashActivity srcActivity = this.mActivityRef.get();
+            if(srcActivity == null) {
+                return;
+            }
+            switch (msg.what) {
+                case GO_AHEAD_WHAT:
+                    long elapsedTime = SystemClock.uptimeMillis() - srcActivity.mStartTime;
+                    if(elapsedTime >= INTERVAL && !srcActivity.mIsDone) {
+                        srcActivity.mIsDone = true;
+                        srcActivity.goAhead();
+                    }
+                    break;
+            }
+        }
+    };
+
+    private void goAhead() {
+        final Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mStartTime == -1L) {
+            mStartTime = SystemClock.uptimeMillis();
+        }
+        final Message goAheadMessage = mHandler.obtainMessage(GO_AHEAD_WHAT);
+        mHandler.sendMessageAtTime(goAheadMessage, mStartTime + INTERVAL);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+        if(savedInstanceState != null) {
+            this.mStartTime = savedInstanceState.getLong("START_TIME_KEY");
+        }
+        mHandler = new UIHandler(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("IS_DONE_KEY", mIsDone);
+        outState.putLong("START_TIME_KEY", mStartTime);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        this.mIsDone = savedInstanceState.getBoolean("IS_DONE_KEY");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
+}
